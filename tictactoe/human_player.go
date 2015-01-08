3eddfe3325798b330e91aafc8f1c7a189a48a36a
Jayden Sung <sung.jayden@gmail.com>
2015-01-07 17:02:59 -0800

    added ui to Human struct to pass in #AskForPlayerMove

--- tictactoe/human_player.go
+++ tictactoe/human_player.go
@@ -1,19 +1,26 @@
 package tictactoe
 
 type Human struct {
-  marker string
-  move int
+	marker  string
+	move    int
+	console UI
+}
+
+func NewHumanPlayer() *Human {
+	human := new(Human)
+	human.console = new(ConsoleUI)
+	return human
 }
 
 func (human *Human) SetMarker(marker string) {
-  human.marker = marker
+	human.marker = marker
 }
 
 func (human *Human) Marker() string {
-  return human.marker
+	return human.marker
 }
 
 func (human *Human) Move(board *Board) int {
-  human.move = GetInput()
-  return human.move
+	human.move = human.console.AskForPlayerMove()
+	return human.move
 }
--- tictactoe/human_player_test.go
+++ tictactoe/human_player_test.go
@@ -1,36 +1,36 @@
 package tictactoe
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestHumanIsAPlayer(t *testing.T) {
-  human := new(Human)
-  var player Player = human
+	human := NewHumanPlayer()
+	var player Player = human
 
-  t.Log("Human implements the Player interface")
-  assert.Equal(t, human, player)
+	t.Log("Human implements the Player interface")
+	assert.Equal(t, human, player)
 }
 
 func TestManipulateHumanMark(t *testing.T) {
-  human := new(Human)
-  t.Log("gets and sets the marker of human player")
-  human.SetMarker("X")
-  assert.Equal(t, "X", human.Marker())
+	human := NewHumanPlayer()
+	t.Log("gets and sets the marker of human player")
+	human.SetMarker("X")
+	assert.Equal(t, "X", human.Marker())
 
-  t.Log("changes the marker of human player")
-  human.SetMarker("O")
-  assert.Equal(t, "O", human.Marker())
+	t.Log("changes the marker of human player")
+	human.SetMarker("O")
+	assert.Equal(t, "O", human.Marker())
 }
 
 func TestMakesMove(t *testing.T) {
-  human := new(Human)
-  mockMove := "1"
-  writeMockInput(mockMove)
-  board := MakeBoard()
+	human := NewHumanPlayer()
+	mockMove := "1"
+	writeMockInput(mockMove)
+	board := MakeBoard()
 
-  t.Log("captures user move selection")
-  human.Move(board)
-  assert.Equal(t, 1, human.move)
+	t.Log("captures user move selection")
+	human.Move(board)
+	assert.Equal(t, 1, human.move)
 }
