f0c40d7b062625c35f8c57ff3e68e4e84a562bc1
Jayden Sung <sung.jayden@gmail.com>
2015-01-08 11:48:40 -0800

    added depthLimit to speed up search and deleted dead test

--- tictactoe/perfect_player.go
+++ tictactoe/perfect_player.go
@@ -1,80 +1,89 @@
 package tictactoe
 
 const (
-  initialDepth = 0
-  maxScore = 10
+	initialDepth = 0
+	depthLimit   = 6
+	maxScore     = 10
 )
 
 type PerfectPlayer struct {
-  marker string
-  move int
+	marker string
+	move   int
+}
+
+func NewPerfectPlayer() *PerfectPlayer {
+	return new(PerfectPlayer)
 }
 
 func (player *PerfectPlayer) Marker() string {
-  return player.marker
+	return player.marker
 }
 
 func (player *PerfectPlayer) SetMarker(marker string) {
-  player.marker = marker
+	player.marker = marker
 }
 
 func (player *PerfectPlayer) Move(board *Board) int {
-  player.minimax(board, player.marker, initialDepth)
-  return player.move
+	player.minimax(board, player.marker, initialDepth)
+	return player.move
 }
 
 func (player *PerfectPlayer) minimax(board *Board, marker string, depth int) int {
-  if board.GameOver() { return player.getEvaluatedScore(board, depth) }
+	if board.GameOver() || depth == depthLimit {
+		return player.getEvaluatedScore(board, depth)
+	}
 
-  availableMoves := board.GetAvailableMoves()
-  depth++
-  scores := make(map[int]int, len(availableMoves))
+	availableMoves := board.GetAvailableMoves()
+	depth++
+	scores := make(map[int]int, len(availableMoves))
 
-  for _,move := range availableMoves {
-    board.FillSpace(move, marker)
-    nextTurn := getNextTurn(marker)
-    scores[move] = player.minimax(board, nextTurn, depth)
-    board.ClearSpace(move)
-  }
+	for _, move := range availableMoves {
+		board.FillSpace(move, marker)
+		nextTurn := getNextTurn(marker)
+		scores[move] = player.minimax(board, nextTurn, depth)
+		board.ClearSpace(move)
+	}
 
-  return player.getBestScoreForMarker(marker, scores)
+	return player.getBestScoreForMarker(marker, scores)
 }
 
 func (player *PerfectPlayer) getBestScoreForMarker(marker string, scores map[int]int) int {
-  var bestScore int
-  if player.Marker() == marker {
-    player.move, bestScore = getBestMove(scores, maxScore)
-  } else {
-    _,bestScore = getBestMove(scores, -maxScore)
-  }
-  return bestScore
+	var bestScore int
+	if player.Marker() == marker {
+		player.move, bestScore = getBestMove(scores, maxScore)
+	} else {
+		_, bestScore = getBestMove(scores, -maxScore)
+	}
+	return bestScore
 }
 
 func getBestMove(scores map[int]int, targetScore int) (int, int) {
-  bestScore := targetScore * -1
-  var bestMove int
-  for k,v := range scores {
-    if (targetScore > 0 && v > bestScore) || (targetScore < 0 && v < bestScore) {
-      bestScore = v
-      bestMove = k
-    }
-  }
-  return bestMove, bestScore
+	bestScore := targetScore * -1
+	var bestMove int
+	for k, v := range scores {
+		if (targetScore > 0 && v > bestScore) || (targetScore < 0 && v < bestScore) {
+			bestScore = v
+			bestMove = k
+		}
+	}
+	return bestMove, bestScore
 }
 
 func getNextTurn(marker string) string {
-  if marker == "X" { return "O" }
-  return "X"
+	if marker == "X" {
+		return "O"
+	}
+	return "X"
 }
 
 func (player *PerfectPlayer) getEvaluatedScore(board *Board, depth int) int {
-  winner, gameWon := board.GameWon()
-  if gameWon {
-    if winner == player.marker {
-      return maxScore - depth
-    } else {
-      return depth - maxScore
-    }
-  }
-  return 0
+	winner, gameWon := board.GameWon()
+	if gameWon {
+		if winner == player.marker {
+			return maxScore - depth
+		} else {
+			return depth - maxScore
+		}
+	}
+	return 0
 }
--- tictactoe/perfect_player_test.go
+++ tictactoe/perfect_player_test.go
@@ -1,66 +1,52 @@
 package tictactoe
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestPerfectPlayerIsAPlayer(t *testing.T) {
-  perfectPlayer := new(PerfectPlayer)
-  var player Player = perfectPlayer
+	perfectPlayer := new(PerfectPlayer)
+	var player Player = perfectPlayer
 
-  t.Log("PerfectPlayer implements the Player interface")
-  assert.True(t, player == perfectPlayer)
+	t.Log("PerfectPlayer implements the Player interface")
+	assert.True(t, player == perfectPlayer)
 }
 
 func TestPerfectPlayerCanManipulateMarker(t *testing.T) {
-  perfectPlayer := new(PerfectPlayer)
-  perfectPlayer.SetMarker("O")
+	perfectPlayer := new(PerfectPlayer)
+	perfectPlayer.SetMarker("O")
 
-  t.Log("PerfectPlayer can set its mark")
-  assert.Equal(t, "O", perfectPlayer.Marker())
+	t.Log("PerfectPlayer can set its mark")
+	assert.Equal(t, "O", perfectPlayer.Marker())
 }
 
 func TestPerfectPlayerGetsBestMoves(t *testing.T) {
-  perfectPlayer := new(PerfectPlayer)
-  perfectPlayer.SetMarker("O")
+	perfectPlayer := new(PerfectPlayer)
+	perfectPlayer.SetMarker("O")
 
-  t.Log("PerfectPlayer should return winning move")
-  assert.Equal(t, 2, perfectPlayer.Move(testBoard()))
+	t.Log("PerfectPlayer should return winning move")
+	assert.Equal(t, 2, perfectPlayer.Move(testBoard()))
 
-  t.Log("PerfectPlayer should block winning move")
-  assert.Equal(t, 8, perfectPlayer.Move(blockBoard()))
+	t.Log("PerfectPlayer should block winning move")
+	assert.Equal(t, 8, perfectPlayer.Move(blockBoard()))
 
-  t.Log("PerfectPlayer should play center space")
-  firstMoveBoard := MakeBoard()
-  firstMoveBoard.FillSpace(0, "X")
-  assert.Equal(t, 4, perfectPlayer.Move(firstMoveBoard))
-
-  //t.Log("PerfectPlayer plays corner spaces if first")
-  //blankBoard := MakeBoard()
-  //perfectPlayer.SetMarker("X")
-  //corners := []int{0, 2, 6, 8}
-  //move := perfectPlayer.GetMove(blankBoard)
-  //assert.True(t, valueInSlice(move, corners))
-}
-
-func valueInSlice(value int, slice []int) bool {
-  for _,v := range slice {
-    if v == value { return true }
-  }
-  return false
+	t.Log("PerfectPlayer should play center space")
+	firstMoveBoard := NewBoard()
+	firstMoveBoard.FillSpace(0, "X")
+	assert.Equal(t, 4, perfectPlayer.Move(firstMoveBoard))
 }
 
 func blockBoard() *Board {
-  board := MakeBoard()
-  FillSpaces(board, "X", 0, 5, 6, 7)
-  FillSpaces(board, "O", 1, 3, 4)
-  return board
+	board := NewBoard()
+	FillSpaces(board, "X", 0, 5, 6, 7)
+	FillSpaces(board, "O", 1, 3, 4)
+	return board
 }
 
 func testBoard() *Board {
-  board := MakeBoard()
-  FillSpaces(board, "X", 0, 3, 7)
-  FillSpaces(board, "O", 4, 6)
-  return board
+	board := NewBoard()
+	FillSpaces(board, "X", 0, 3, 7)
+	FillSpaces(board, "O", 4, 6)
+	return board
 }
